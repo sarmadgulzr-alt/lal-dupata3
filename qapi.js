21aa6e08e87b2fed94fe145c84c0ef258f07c5aa
sarmadgulzr-alt <sarmadgulzr@gmail.com>
2025-11-29 12:52:45 +0500

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"692aa6082d10660018bac3c4",
+				"692aa6082d10660018bac3c7"
+			]
+		},
+		"692aa6082d10660018bac3c4": {
+			"id": "692aa6082d10660018bac3c4",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"692aa6082d10660018bac3c7": {
+			"id": "692aa6082d10660018bac3c7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
